fd175e8cb42c041b863befb995f555403254c871
Bartek <barteks890@gmail.com>
2015-11-20 20:43:33 +0100

    Dodanie menu bocznego

--- Bling/www/app/app.js
+++ Bling/www/app/app.js
@@ -1,28 +1,44 @@
-﻿var app = angular.module('app', ['ionic']);
+﻿angular.module('app', ['ionic'])
 
-app.config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        // if (window.cordova && window.cordova.plugins.Keyboard) {
+        // cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        // cordova.plugins.Keyboard.disableScroll(true);
 
- 
-    $stateProvider.state('home', {
-        url: '/home',
-        views: {
-            home: {
-                templateUrl: 'home.html'
-            }
-        }
+        //  }
+        //  if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        //   StatusBar.styleDefault();
+        //}
     });
+})
 
-    $stateProvider.state('help', {
-        url: '/help',
-        views: {
-            help: {
-                templateUrl: 'help.html'
-            }
-        }
-    });
+.config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+    $ionicConfigProvider.tabs.position('bottom'); //bottom
+
+
+    $stateProvider
+     .state('app', {
+         url: "/app",
+         abstract: true,
+         templateUrl: "app/dashboard/partials/dashboard-layout.html"
+         //                    controller: 'AppCtrl'
+     })
+     .state('app.playlists', {
+         url: "/playlists",
+         views: {
+             'menuContent': {
+                 templateUrl: "app/dashboard/partials/dashboard-list.html"
+                 //	                        controller: 'PlaylistsCtrl'
+             }
+         }
+     });
+
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/playlists');
 });
-
-
-
-
