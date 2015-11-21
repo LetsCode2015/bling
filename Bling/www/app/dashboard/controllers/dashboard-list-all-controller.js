4be09760dda774aa2f4783b50beb05f8c98f1f64
Bartek <barteks890@gmail.com>
2015-11-21 10:06:07 +0100

    fix konflktow

--- Bling/www/app/dashboard/controllers/dashboard-list-all-controller.js
+++ Bling/www/app/dashboard/controllers/dashboard-list-all-controller.js
@@ -3,11 +3,7 @@
 module.controller('dashboard-list-all-controller', function ($scope, $state, boardService) {
     $scope.test = "hello world";
 
-<<<<<<< HEAD
     boardService.getAll("all", function (dashboards) {
-=======
-    boardService.getAll(function (dashboards) {
->>>>>>> 43a158746033426de5e52bf9cd0a697180230b78
         $scope.dashboards = dashboards;
     });
 
