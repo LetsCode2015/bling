5e7f47059b1894e4988cd815c1b19709096654a7
Bartek <barteks890@gmail.com>
2015-11-21 01:51:06 +0100

    prace nad board + podlaczenie angular-resource

--- Bling/www/app/dashboard/controllers/dashboard-list-all-controller.js
+++ Bling/www/app/dashboard/controllers/dashboard-list-all-controller.js
@@ -1,8 +1,13 @@
 ﻿var module = angular.module('app.dashboard');
 
-module.controller('dashboard-list-all-controller', function ($scope, boardService) {
+module.controller('dashboard-list-all-controller', function ($scope, $state, boardService) {
 
     $scope.test = "hello world";
 
     $scope.dashboards = boardService.getBoards("all");
+
+    $scope.showBoard = function (boardId) {
+        $state.go('board.details');
+//        $state.go('board.details', { boardId: boardId });
+    }
 });
--- Bling/www/app/shared/services/boardService.js
+++ Bling/www/app/shared/services/boardService.js
@@ -8,26 +8,32 @@ module.factory('boardService', function () {
             var description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat';
             return [
                 {
+                    id: 0,
                     title: 'Dashboard1' + category,
                     description: description
                 },
                 {
+                    id: 1,
                     title: 'Dashboard2' + category,
                     description: description
                 },
                 {
+                    id: 2,
                     title: 'Dashboard3' + category,
                     description: description
                 },
                 {
+                    id: 3,
                     title: 'Dashboard4' + category,
                     description: description
                 },
                 {
+                    id: 4,
                     title: 'Dashboard5' + category,
                     description: description
                 },
                 {
+                    id: 5,
                     title: 'Dashboard6' + category,
                     description: description
                 }
