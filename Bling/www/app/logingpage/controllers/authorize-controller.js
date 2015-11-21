e24738be578e2d94519380f9e04d15367e1487da
Zeddiboy <zeddicus1@wp.pl>
2015-11-21 07:18:05 +0100

    commit przed pullem

---
+++ Bling/www/app/logingpage/controllers/authorize-controller.js
@@ -0,0 +1,22 @@
+﻿var module = angular.module("app.loginpage");
+
+module.controller("authorize-controller", function($scope, $cordovaOauth) {
+
+    $scope.authorizationResult = {
+        successs: false,
+        message: "",
+        token: ""
+    };
+
+    $scope.authenticate = function() {
+        $cordovaOauth.google("809124329049-m2c5suoop2pqg5m063lijppf4klqvvun.apps.googleusercontent.com", ["email"])
+            .then(function (result) {
+                $scope.authorizationResult = { successs: true, message: "Succesfully authorized", token: JSON.stringify(result) };
+                console.log("Response Object -> " + JSON.stringify(result));
+                
+            }, function (error) {
+                $scope.authorizaitonResult = {success: false,message: "Sorry... Unable to authorize"}
+                console.log("Error -> " + error);
+            });
+    };
+});
