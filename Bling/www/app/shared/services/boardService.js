3c3e1146381760bc2eb96a0ee318c7773be114e5
Patryk Mikulicz <elmicool@gmail.com>
2015-11-21 03:51:10 +0100

    poprawa na male litery

--- Bling/www/app/shared/services/boardService.js
+++ Bling/www/app/shared/services/boardService.js
@@ -11,11 +11,15 @@ module.factory('boardService', function ($http) {
             else
                 $http.get('http://localhost:50313/board/getall')
                     .then(function (success) { successCallback(success.data); });
-                },
+        },
         getBoard: function (boardId, successCallback) {
             $http.get('http://localhost:50313/board/get/' + boardId)
                 .then(function (success) { successCallback(success.data); });
-                },
+        },
+        addBoard: function (newBoard, successCallback) {
+            $http.post('http://localhost:50313/board/create', newBoard)
+                .then(function (success) { successCallback(success.data); });
+        },
     };
     // Our first service
     return serviceInstance;
